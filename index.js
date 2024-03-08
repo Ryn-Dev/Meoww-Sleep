6eb0359b6737aec5cb0ef1b2b1ae36787ea80c39
Febryan1453 <brayenfebryan@gmail.com>
2024-03-08 17:35:08 +0700

    Meooww Sleep

---
+++ index.js
@@ -0,0 +1,14 @@
+const febArr = ['Febryan', 'Makan', 'Ngoding', 'Tidur', 'Bangun','Repeat'];
+console.log(febArr);
+console.table(febArr);
+
+const febObj = {
+    name: 'Febryan',
+    makan: 'Makan',
+    ngoding: 'Ngoding',
+    tidur: 'Tidur',
+    bangun: 'Bangun',
+    repeat: 'Repeat'
+}
+console.log(febObj);
+console.table(febObj);
